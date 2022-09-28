d3f5bf16625e6a03fa4d721d41038441efd0b713
Tuyn Doan <admin@tuyn.info>
2022-09-29 01:31:53 +0700

    https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-json/

--- models/example/accounts.sql
+++ models/example/accounts.sql
@@ -8,17 +8,18 @@
 {{ config(materialized='table') }} 
 
 with source_data as (
-     SELECT _airbyte_data -> 'Id' AS id,
-        _airbyte_data -> 'Code' AS code,
-         _airbyte_data -> 'Name' AS name,
-          _airbyte_data -> 'IsCash' AS iscash,
-           _airbyte_data -> 'Prefix' AS prefix,
-           _airbyte_data -> 'Inactive' AS inactive,
-            _airbyte_data -> 'Description' AS description
-    FROM _airbyte_raw_acc_account
+     SELECT _airbyte_data ->> 'Id' AS id,
+        _airbyte_data ->> 'Code' AS code,
+         _airbyte_data ->> 'Name' AS name,
+          _airbyte_data ->> 'IsCash' AS iscash,
+           _airbyte_data ->> 'Prefix' AS prefix,
+           _airbyte_data ->> 'Inactive' AS inactive,
+            _airbyte_data ->> 'Description' AS description
+FROM _airbyte_raw_acc_account
 )
 
-select id, code, name from source_data
+select id, code, name
+from source_data
 
 /*
     Uncomment the line below to remove records with null `id` values
